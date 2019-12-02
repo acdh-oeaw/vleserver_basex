0e09ba1d427e165005995b79594ff75f83d85bfb
Omar Siam <simar@gmx.net>
2019-12-02 18:48:17 +0100

    Change name to match the rest of the test files 2.

--- test.js
+++ test.js
@@ -11,7 +11,7 @@ require('./test/utilSetup')
 describe('WDE REST API', function() {
     this.timeout(20000);
     describe('Dictionary listing and creation', require('./test/dicts-test').curry(baseURI, basexAdminUser, basexAdminPW));
-    describe('Dictionary management', require('./test/dicts-{dicts_name}-test').curry(baseURI, basexAdminUser, basexAdminPW));
+    describe('Dictionary management', require('./test/dicts-{dict_name}-test').curry(baseURI, basexAdminUser, basexAdminPW));
     describe('User management - first part', require('./test/dicts-{dict_name}-users-test').curry(baseURI, basexAdminUser, basexAdminPW));
     describe('User management - second part',require('./test/dicts-{dict_name}-users-{users_id}-test').curry(baseURI, basexAdminUser, basexAdminPW));
     describe('Dictionary data creation', require('./test/dicts-{dict_name}-entries-test').curry(baseURI, basexAdminUser, basexAdminPW));
